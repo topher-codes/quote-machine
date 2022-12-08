3cd9a54e595110b99ea040cce30ef39d847fa580
Chris <ghost@drednaut.net>
2022-12-07 20:18:16 -0800

    Passed all tests
    Signed-off-by: Chris <ghost@drednaut.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { useState } from 'react';
-import './App.css';
+import axios from 'axios';
 
 function App() {
+	const [joke, setJoke] = useState('Click the button for a joke');
+	const [author, setAuthor] = useState('-Author');
+	const handleClick = async () => {
+		const joke = await axios.get('https://api.chucknorris.io/jokes/random');
+		const name = await axios.get('https://randomuser.me/api/');
+		setAuthor(name.data.results[0].name.first);
+		setJoke(joke.data.value);
+	};
+
 	return (
 		<div className="wrapper">
 			<div id="quote-box">
-				<div id="text">Text</div>
-				<div id="author">Author</div>
+				<div id="text">{joke}</div>
+				<div id="author">-{author}</div>
 				<a href="https://twitter.com/intent/tweet" id="tweet-quote">
 					Tweet Quote
 				</a>
-				<button id="new-quote">New Quote</button>
+				<button id="new-quote" onClick={handleClick}>
+					New Quote
+				</button>
 			</div>
 		</div>
 	);
